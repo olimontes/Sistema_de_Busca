1128eb4116e3b9c2f5819ee4e6115d81100d2a4d
Frank Gabriel <fgabrieloliveira1@gmail.com>
2025-08-06 23:14:00 -0300

    algoritmo final

--- projeto.cpp
+++ projeto.cpp
@@ -132,23 +132,6 @@ void buscaAhoCorasick(const string &texto, const vector<string> &padroes)
 }
 
 
-// ---------- DISTANCIA DE EDICAO ----------
-int distanciaLevenshtein(const string &a, const string &b)
-{
-    int m = a.size(), n = b.size();
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-    for (int i = 0; i <= m; ++i)
-        dp[i][0] = i;
-    for (int j = 0; j <= n; ++j)
-        dp[0][j] = j;
-
-    for (int i = 1; i <= m; ++i)
-        for (int j = 1; j <= n; ++j)
-            dp[i][j] = min({dp[i - 1][j] + 1,
-                            dp[i][j - 1] + 1,
-                            dp[i - 1][j - 1] + (a[i - 1] != b[j - 1])});
-    return dp[m][n];
-}
 
 // ---------- ORTOGRAFIA ----------
 set<string> carregarDicionario(const string &arquivo)
